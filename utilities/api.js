13bb94550700dd04fe59604f02ba3519f35e2bf9
Francis Bulus <notchera@gmail.com>
2021-08-05 17:54:58 +0100

    create and export axios instance

--- utilities/api.js
+++ utilities/api.js
@@ -1,8 +1,11 @@
-export default function axiosWithAuth() {
+import axios from "axios";
+const isBrowser = () => typeof window !== "undefined";
+export function api() {
   const instance = axios.create({
     baseURL: "http://api.editmode.com",
     headers: {
       "Content-Type": "application/json",
+      referrer: isBrowser() ? window.location.href : "",
     },
   });
 
