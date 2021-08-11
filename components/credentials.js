87ef7b51fe08a11e4206d912bd4eb41b7c80c69d
Francis Bulus <notchera@gmail.com>
2021-08-11 11:52:09 +0100

    add validation to sign up

--- components/credentials.js
+++ components/credentials.js
@@ -2,6 +2,7 @@ import { useCookie } from "../utilities";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
+import { Formik, Form, Field } from "formik";
 
 const initialFormState = {
   email: "",
@@ -18,43 +19,43 @@ const UserCredentials = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoginValid, setIsLoginValid] = useState(true);
   const router = useRouter();
-  const handleChange = (e) => {
+  const handleFormChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleLogIn = async (e) => {
     e.preventDefault();
-    if (authType === "login") {
-      const url = `https://api.editmode.com/login?email=${credentials.email}&password=${credentials.password}`;
-      setIsLoading(true);
-      const res = await fetch(url, { method: "POST" });
-      const data = await res.json();
-      if (data.errors && data.errors[0].includes("does not match")) {
-        setIsLoading(false);
-        setIsLoginValid(false);
-      }
-      const token = data.authentication_token;
-      updateCookie(token);
-      if (token) setIsLoading(false);
-      if (token) {
-        setIsLoginValid(true);
-        Cookies.set("em_user_email", credentials.email);
-        router.push("/");
-      }
-    } else {
-      const url = `https://api.editmode.com/users/sign_up?email=${credentials.email}&password=${credentials.password}&first_name=${credentials.firstName}&last_name=${credentials.lastName}&password_confirmation=${credentials.passwordConfirmation}&api_key=z9JrfCcPz3KmjnSMxNKfggKT`;
-      setIsLoading(true);
-      const res = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await res.json();
-      const token = data.authentication_token;
-      updateCookie(token);
-      if (token) setIsLoading(false);
-      Cookies.set("em_user_email", credentials.email);
-      if (data.authentication_token) router.push("/");
+    const url = `https://api.editmode.com/login?email=${credentials.email}&password=${credentials.password}`;
+    setIsLoading(true);
+    const res = await fetch(url, { method: "POST" });
+    const data = await res.json();
+    if (data.errors && data.errors[0].includes("does not match")) {
+      setIsLoading(false);
+      setIsLoginValid(false);
     }
+    const token = data.authentication_token;
+    updateCookie(token);
+    if (token) setIsLoading(false);
+    if (token) {
+      setIsLoginValid(true);
+      Cookies.set("em_user_email", credentials.email);
+      router.push("/");
+    }
+  };
+
+  const handleSignUp = async (values) => {
+    const url = `https://api.editmode.com/users/sign_up?email=${values.email}&password=${values.password}&first_name=${values.firstName}&last_name=${values.lastName}&password_confirmation=${values.passwordConfirmation}&api_key=z9JrfCcPz3KmjnSMxNKfggKT`;
+    setIsLoading(true);
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    const token = data.authentication_token;
+    updateCookie(token);
+    if (token) setIsLoading(false);
+    Cookies.set("em_user_email", values.email);
+    if (data.authentication_token) router.push("/");
   };
 
   const handleAuthTypeSwitch = (e) => {
@@ -95,7 +96,7 @@ const UserCredentials = () => {
           <div className="container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300">
             <form
               className="py-12 p-10 bg-white rounded-xl"
-              onSubmit={handleSubmit}
+              onSubmit={handleLogIn}
             >
               <div
                 class={`rounded-md bg-red-100 p-4 mb-6 ${
@@ -137,7 +138,7 @@ const UserCredentials = () => {
                   placeholder="you@example.com"
                   name="email"
                   value={credentials.email}
-                  onChange={handleChange}
+                  onChange={handleFormChange}
                 />
               </div>
               <div className="">
@@ -153,7 +154,7 @@ const UserCredentials = () => {
                   placeholder=""
                   name="password"
                   value={credentials.password}
-                  onChange={handleChange}
+                  onChange={handleFormChange}
                 />
               </div>
               <button
@@ -220,123 +221,205 @@ const UserCredentials = () => {
             </p>
           </div>
           <div className="container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300">
-            <form
-              className="py-12 p-10 bg-white rounded-xl"
-              onSubmit={handleSubmit}
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+                passwordConfirmation: "",
+              }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = "Your email is equired";
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                  errors.email = "Invalid email address";
+                } else if (!values.password) {
+                  errors.password = "A password is required";
+                } else if (values.password.length < 8) {
+                  errors.password =
+                    "Password must be at least 8 characters long";
+                } else if (!values.passwordConfirmation) {
+                  errors.passwordConfirmation =
+                    "You need to confirm your password";
+                } else if (
+                  values.passwordConfirmation &&
+                  values.password !== values.passwordConfirmation
+                ) {
+                  errors.passwordConfirmation = "Your passwords do not match";
+                }
+                return errors;
+              }}
+              onSubmit={(values) => {
+                handleSignUp(values);
+              }}
             >
-              {" "}
-              <div className="mb-6">
-                <label
-                  className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
-                  htmlFor="firstName"
+              {({ values, errors, touched, handleChange, handleSubmit }) => (
+                <form
+                  className="py-12 p-10 bg-white rounded-xl"
+                  onSubmit={handleSubmit}
                 >
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                  placeholder="John"
-                  name="firstName"
-                  value={credentials.firstName}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
-                  htmlFor="lastName"
-                >
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                  placeholder="Doe"
-                  name="lastName"
-                  value={credentials.lastName}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  type="text"
-                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                  placeholder="you@example.com"
-                  name="email"
-                  value={credentials.email}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="">
-                <label
-                  className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                  placeholder=""
-                  name="password"
-                  value={credentials.password}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="">
-                <label
-                  className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
-                  htmlFor="passwordConfirmation"
-                >
-                  Password confirmation
-                </label>
-                <input
-                  type="password"
-                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                  placeholder=""
-                  name="passwordConfirmation"
-                  value={credentials.passwordConfirmation}
-                  onChange={handleChange}
-                />
-              </div>
-              <button
-                className={`flex justify-center w-full mt-6 text-indigo-50 font-medium py-2 leading-6 px-4 rounded-md hover:bg-indigo-400 transition duration-300 button ${
-                  isLoading
-                    ? `cursor-not-allowed bg-indigo-400`
-                    : `bg-indigo-500`
-                }`}
-                type="submit"
-              >
-                <svg
-                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white mt-0.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  style={{ display: isLoading ? "block" : "none" }}
-                >
-                  <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                  ></circle>
-                  <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                {isLoading ? "Signing Up" : "Sign Up"}
-              </button>
-            </form>
+                  {" "}
+                  <div className="mb-6">
+                    <label
+                      className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
+                      htmlFor="firstName"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                      placeholder="John"
+                      name="firstName"
+                      value={values.firstName}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
+                      htmlFor="lastName"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                      placeholder="Doe"
+                      name="lastName"
+                      value={values.lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="text"
+                      className={`border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-1  rounded ${
+                        errors.email && touched.email
+                          ? "border-red-400 focus:ring-red-500"
+                          : "focus:ring-indigo-400"
+                      }`}
+                      placeholder="you@example.com"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                    />
+                    <div
+                      className={`text-sm text-red-400 -mb-3 ${
+                        errors.email && touched.email ? "" : "invisible"
+                      }`}
+                    >
+                      {errors.email && touched.email && errors.email
+                        ? errors.email
+                        : "&nbsp"}
+                    </div>
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
+                      htmlFor="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className={`border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-1 rounded ${
+                        errors.password && touched.password
+                          ? "border-red-400 focus:ring-red-500"
+                          : "focus:ring-indigo-400"
+                      }`}
+                      placeholder=""
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                    />
+                    <div
+                      className={`text-sm text-red-400 -mb-3 ${
+                        errors.password && touched.password ? "" : "invisible"
+                      }`}
+                    >
+                      {errors.password && touched.password && errors.password
+                        ? errors.password
+                        : "&nbsp"}
+                    </div>
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="mr-4 text-gray-700 text-sm font-medium inline-block mb-2"
+                      htmlFor="passwordConfirmation"
+                    >
+                      Password confirmation
+                    </label>
+                    <input
+                      type="password"
+                      className={`border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-1 rounded ${
+                        errors.passwordConfirmation &&
+                        touched.passwordConfirmation
+                          ? "border-red-400 focus:ring-red-500"
+                          : "focus:ring-indigo-400"
+                      }`}
+                      placeholder=""
+                      name="passwordConfirmation"
+                      value={values.passwordConfirmation}
+                      onChange={handleChange}
+                    />
+                    <div
+                      className={`text-sm text-red-400 -mb-3  ${
+                        errors.passwordConfirmation &&
+                        touched.passwordConfirmation
+                          ? ""
+                          : "invisible"
+                      }`}
+                    >
+                      {errors.passwordConfirmation &&
+                      touched.passwordConfirmation &&
+                      errors.passwordConfirmation
+                        ? errors.passwordConfirmation
+                        : "&nbsp"}
+                    </div>
+                  </div>
+                  <button
+                    className={`flex justify-center w-full mt-6 text-indigo-50 font-medium py-2 leading-6 px-4 rounded-md hover:bg-indigo-400 transition duration-300 button ${
+                      isLoading
+                        ? `cursor-not-allowed bg-indigo-400`
+                        : `bg-indigo-500`
+                    }`}
+                    type="submit"
+                  >
+                    <svg
+                      class="animate-spin -ml-1 mr-3 h-5 w-5 text-white mt-0.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      style={{ display: isLoading ? "block" : "none" }}
+                    >
+                      <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    {isLoading ? "Signing Up" : "Sign Up"}
+                  </button>
+                </form>
+              )}
+            </Formik>
           </div>
         </>
       )}
