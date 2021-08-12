18a6d0ee07970a067afe13a9f2c47afcfb22ffd7
Francis Bulus <notchera@gmail.com>
2021-08-12 10:15:01 +0100

    _

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Layout from "components/layout";
+import Dashboard from "components/dashboard";
 import { useCookie } from "utilities";
-import Selection from "components/select";
-import Loader from "react-loader-spinner";
 import { defaultOption } from "../utilities";
+import Auth from "components/credentials";
 
 export default function CallbackPage() {
   const router = useRouter();
@@ -19,10 +18,11 @@ export default function CallbackPage() {
   const [isInstalling, setIsInstalling] = useState(false);
   const [isFetchingEditmodeProjects, setIsFetchingEditmodeProjects] =
     useState(false);
+  const [authenticated, setAuthenticated] = useState("auth");
 
   useEffect(() => {
     if (!value) {
-      router.push("/authentication");
+      setAuthenticated("auth");
     } else setEditmodeToken(value);
   }, [router, value]);
 
@@ -149,57 +149,17 @@ export default function CallbackPage() {
   if (!value) return null;
 
   return (
-    <Layout>
-      <div className="w-full max-w-2xl divide-y">
-        {isFetchingEditmodeProjects && (
-          <div className="py-4 flex justify-center align-center">
-            <Loader type="TailSpin" color="#616AE9" height={100} width={100} />
-          </div>
-        )}
-        {userEditmodeProjects[0] && !isFetchingEditmodeProjects && (
-          <>
-            <section>
-              <Selection
-                projectOptions={userEditmodeProjects}
-                setProjectToInstall={setProjectToInstall}
-              />
-            </section>
-            <section className="py-4">
-              <button
-                className={`flex justify-center w-full mt-6 text-white font-medium py-3 leading-6 px-4 rounded-md hover:bg-indigo-400 transition duration-200 button ${
-                  isInstalling
-                    ? `cursor-not-allowed bg-indigo-300`
-                    : `bg-indigo-500`
-                }`}
-                onClick={handleInstall}
-              >
-                <svg
-                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white mt-0.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  style={{ display: isInstalling ? "block" : "none" }}
-                >
-                  <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                  ></circle>
-                  <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                {isInstalling ? "ADDING INTEGRATION" : "ADD INTEGRATION"}
-              </button>
-            </section>
-          </>
-        )}
-      </div>
-    </Layout>
+    <>
+      {!value && <Auth />}
+      {value && (
+        <Dashboard
+          userEditmodeProjects={userEditmodeProjects}
+          handleInstall={handleInstall}
+          isFetchingEditmodeProjects={isFetchingEditmodeProjects}
+          isInstalling={isInstalling}
+          setProjectToInstall={setProjectToInstall}
+        />
+      )}
+    </>
   );
 }
