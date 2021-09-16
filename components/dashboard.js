d1008a4e580fdc89e7e72f0bdf13fa503fe858d6
Francis Bulus <notchera@gmail.com>
2021-09-16 19:50:51 +0100

    change button text for config page

--- components/dashboard.js
+++ components/dashboard.js
@@ -267,11 +267,13 @@ export default function Dashboard({
                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                 ></path>
               </svg>
-              {isInstalling
-                ? "ADDING INTEGRATION"
-                : dashboardView === "deploy"
-                ? "ADD INTEGRATION TO NEW TEMPLATE"
-                : "ADD INTEGRATION"}
+              {!isConfiguration
+                ? isInstalling
+                  ? "ADDING INTEGRATION"
+                  : dashboardView === "deploy"
+                  ? "ADD INTEGRATION TO NEW TEMPLATE"
+                  : "ADD INTEGRATION"
+                : "SAVE CHANGES"}
             </button>
           </section>
         )}
