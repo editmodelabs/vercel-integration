9a762de09f8af9c39939721142f41cf8ae6d4e0b
Francis Bulus <notchera@gmail.com>
2021-09-22 08:24:37 +0100

    _

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,6 @@ export default function CallbackPage() {
         currentProjectId
       );
       const accessTokenRes = await persistAccessToken();
-      alert(JSON.stringify(accessTokenRes));
       if (json.value && accessTokenRes) {
         setHasCloned(true);
         reroute();
@@ -160,10 +159,10 @@ export default function CallbackPage() {
     else setDashboardView("add");
     const fetchAccessToken = async (code) => {
       const details = {
-        client_id: "oac_KxaKzLl1KakFnclDJURDmQtI",
-        client_secret: "9d72agydqs5x5YHX3wTNP8Iv",
+        client_id: "oac_tgUyWFM6PEvxEkJZCLShaoWI",
+        client_secret: "7vuPPxcqZ1AGNNK89EigX7TG",
         code: code,
-        redirect_uri: "http://localhost8000",
+        redirect_uri: "https://vercel-integration-seven.vercel.app",
       };
       var formBody = [];
       for (var property in details) {
